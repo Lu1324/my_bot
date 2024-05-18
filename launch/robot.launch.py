147b706fa828639eca60d53bfdd49a8896c8a71f
Lu1324 <luisa081002@gmail.com>
2024-05-18 16:30:04 +0200

    changed timeout to timeout_ms, launch wirking

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -9,6 +9,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
+from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
